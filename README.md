c483ab3cbc3a811c8602b6bd3a3ad2d2e925831b
Ellen Furness <elasfern@gmail.com>
2022-03-21 22:19:14 -0600

    3-21-22night

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Unit4-WebAPI-Code-Quiz
+
+### Description 
+
+This project involved making a coding quiz using Javascript.  For the user it is important to establish the length the password and then ask them the type of characters they want, either lower case or upper case letters or symbols or numbers.  In addition, have conditional statements to guide the user along the way with window dialog prompts, alerts and confirm statements.  Prompts notify the user. Alerts can be used here to let the user know they entered the wrong value outside the parameters of the length of the password, in this example, less than 8 or over 128 characters. Window method confirms are just to verify what the user wants, usually true or false and click OK.  If cancel button is clicked it will return null.  
+
+
+## Review
+
+[Deployed Application] 
+https://efurness.github.io/Unit4-WebAPI-Code-Quiz/
+
+[GitHub Repository] 
+https://github.com/efurness/Unit4-WebAPI-Code-Quiz.git
+
+[Project screenshot](password_generator.png) 
+
+## Contact
+
+Ellen Furness - [Ellen.Furness@du.edu]
+
+## Acknowledgments
+
+List of resources and websites
+
+* [Dev community](https://dev.to/)
+* [W3 Resource](https://www.w3resource.com/)
+* [GeeksforGeeks JavaScript Dialog boxes](https://geeksforgeeks.com/)
+* [Tutor](https://bootcampspot.com/)
+* [Code Academy](https://www.codecademy.com/)
+
--- script2.js
+++ script2.js
@@ -5,7 +5,8 @@ var timeEl = document.querySelector(".time");
 // Selects element by id
 var mainEl = document.getElementById("main");
 
-var secondsLeft 
+var secondsLeft
+// score is set to 0 when timer starts
 
 var score = 0;
 function setTime() {
@@ -25,7 +26,7 @@ function setTime() {
   }, 1000);
 }
 
-// Function to create and append colorsplosion image
+// Function to create and append image
 function sendMessage() {
   timeEl.textContent = "time is up! ";
   var imgEl = document.createElement("img");
@@ -33,20 +34,31 @@ function sendMessage() {
   mainEl.appendChild(imgEl);
 
 }
+// counter is set to 0 until quiz starts
 var counter = 0;
+// after start button is clicked it starts quiz
 
 var startButton = document.getElementById("start-button");
 startButton.addEventListener("click", startQuiz)
 function startQuiz() {
-    setTime();
-displayQuestions(counter);
+  setTime();
+  displayQuestions(counter);
 }
+// function display questions as an argument, when counter has gotten to the end of the questions
+// a prompt message comes up to get initials and then give a score
+
 function displayQuestions(arg) {
-  if(counter==Questions.length){
+  if (counter == Questions.length) {
     var initials = prompt("Great job! please enter your initials");
-    alert(initials + ":"+ score);
-    
+    alert(initials + ":" + " Your Score :) " + score);
+    // the items are set to storage
+    localStorage.setItem(initials);
+    localStorage.setItem(score);
+
   }
+    // for each question, the values for each question are in an inner html div element with a ul list item below the question
+  // here javascript is actually creating the elements on the page as they come up 
+
   var questionsDiv = document.getElementById("question");
   questionsDiv.innerHTML = Questions[arg].question;
   var ul = document.createElement("div");
@@ -56,69 +68,78 @@ function displayQuestions(arg) {
   <button class="multiple"value="${Questions[arg].a[2].isCorrect}">${Questions[arg].a[2].text}</button>
   <button class="multiple"value="${Questions[arg].a[3].isCorrect}">${Questions[arg].a[3].text}</button>
   `
-questionsDiv.appendChild(ul);
-var multiple = document.getElementsByClassName("multiple");
-for (let index = 0; index < multiple.length; index++) {
-multiple[index].addEventListener("click",checkAnswer);
-  
-}
-  
-}
-function checkAnswer(event) {
-console.log(event.target.value);
-if(event.target.value=="true") {
-  score++
-}
-else {
-  secondsLeft-=5
-}
+  questionsDiv.appendChild(ul);
+  var multiple = document.getElementsByClassName("multiple");
+  for (let index = 0; index < multiple.length; index++) {
+    multiple[index].addEventListener("click", checkAnswer);
+
+  }
+
 }
 
+// this function checks the answer event and it value is true, adds 1 to the score
+
+function checkAnswer(event) {
+  console.log(event.target.value);
+  if (event.target.value == "true") {
+    score++
+  }
+    // if answer is wrong then 5 seconds is deducted from the timer
+
+  else {
+    secondsLeft -= 5
+  }
+}
+
+// each time the next button is clicked starting at 0 and then adds one displaying next question
 
 const next = document.getElementsByClassName('next')[0];
 var id = 0;
 
 next.addEventListener("click", () => {
   counter++
-displayQuestions(counter)
+  displayQuestions(counter)
 
 })
 
+// for each question it gives the text of the answers and isCorrect is set to true or false, if the answer is correct or not
+
 const Questions = [
   {
-  id: 0,
-  question: "WHAT IS A CLASS IN CSS?",
-  a: [{ text: "attribute", isCorrect: true },
-  { text: "boolean", isCorrect: false },
-  { text: "English", isCorrect: false },
-  { text: "style in css", isCorrect: false }
-  ]
+    id: 0,
+    question: "WHAT IS A CLASS IN CSS?",
+    a: [{ text: "selector", isCorrect: true },
+    { text: "boolean", isCorrect: false },
+    { text: "English", isCorrect: false },
+    { text: "style in css", isCorrect: false }
+    ]
 
-},
-{
-  id: 1,
-  question: "WHAT IS JSON?",
-  a: [{ text: "closed source", isCorrect: false },
-  { text: "open standard file format in web applications", isCorrect: true },
-  { text: "former boyfriend", isCorrect: false },
-  { text: "logo", isCorrect: false }
-  ]
+  },
+  {
+    id: 1,
+    question: "WHAT IS JSON?",
+    a: [{ text: "closed source", isCorrect: false },
+    { text: "open standard file format in web applications", isCorrect: true },
+    { text: "former boyfriend", isCorrect: false },
+    { text: "logo", isCorrect: false }
+    ]
 
-},
-{
-  id: 2,
-  question: "WHAT IS BOOTSTRAP?",
-  a: [{ text: "a piece or leather ", isCorrect: true },
-  { text: "a popular library of code for websites", isCorrect: true },
-  { text: "pull yourself up with them", isCorrect: false },
-  { text: "a brewery in Devide, Colorado", isCorrect: false }
-  ]
-},
-{
-  id: 3,
-  question: "WHAT IS FLEXBOX?",
-  a: [{ text: "allows for a flexble container in websites to fit page", isCorrect: true },
-  { text: "what you can't do after 50", isCorrect: false },
-  { text: "a dance in the 70s'", isCorrect: false },
-  { text: "a logo", isCorrect: false }]}
-  ]
+  },
+  {
+    id: 2,
+    question: "WHAT IS BOOTSTRAP?",
+    a: [{ text: "a piece or leather ", isCorrect: true },
+    { text: "a popular library of code for websites", isCorrect: true },
+    { text: "pull yourself up with them", isCorrect: false },
+    { text: "a brewery in Divide, Colorado", isCorrect: false }
+    ]
+  },
+  {
+    id: 3,
+    question: "WHAT IS FLEXBOX?",
+    a: [{ text: "allows for a flexible containers in websites based on screen size", isCorrect: true },
+    { text: "what you can't do after 50", isCorrect: false },
+    { text: "a dance in the 70s'", isCorrect: false },
+    { text: "a logo", isCorrect: false }]
+  }
+]
